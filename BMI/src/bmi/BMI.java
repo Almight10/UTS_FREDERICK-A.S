07a25538bfea619989a0891ed0593ba802f844de
Almight10 <98369182+Almight10@users.noreply.github.com>
2023-11-29 20:57:07 +0700

    Add files via upload

---
+++ BMI/src/bmi/BMI.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bmi;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class BMI {
+    public static void main(String[] args) {
+       String nama = "Frederick Ananda";
+       int umur = 19;
+       double tb = 1.70;
+       int bb = 85;
+       double BMI;
+       
+       System.out.println("nama: " +nama);
+       System.out.println("umur: " +umur);
+       System.out.println("tb: " +tb);
+       System.out.println("bb: " +bb);
+       
+       BMI= (bb/(tb*tb));
+       
+       if (BMI <18.5){
+           System.out.println(String.format("BMI: %.2f", BMI));
+           System.out.println("Kurus");
+       }
+       else if (BMI <24.9){
+            System.out.println(String.format("BMI: %.2f", BMI));
+            System.out.println("Ideal");
+       }
+       
+       else if (BMI <29.9){
+            System.out.println(String.format("BMI: %.2f", BMI));
+            System.out.println("Gemuk");
+       }
+            
+       else if (BMI <29.9){
+            System.out.println(String.format("BMI: %.2f", BMI));
+            System.out.println("Obesitas");
+       }             
+                   
+              
+                }
+
+}
